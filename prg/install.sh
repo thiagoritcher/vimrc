9fd16780e9a8b3922e27a23d50a1e310b3f0f3f8
Thiago Ritcher <thiagoritcher@gmail.com>
2022-05-20 08:07:08 -0300

    Ajustes vimrc prg

--- prg/install.sh
+++ prg/install.sh
@@ -2,6 +2,6 @@ rm $HOME/.vimrc
 rm $HOME/java.vim
 rm $HOME/java.md
 
-ln -s $PWD/.vimrc $HOME/.vimrc 
+ln -s $PWD/vimrc $HOME/.vimrc 
 ln -s $PWD/java.vim $HOME/java.vim 
 ln -s $PWD/java.md $HOME/java.md 
